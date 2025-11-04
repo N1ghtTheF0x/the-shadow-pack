26bd404b854a64282c8529d431a2dcd0fc5f79de
N1ghtTheF0x <kilo.night.prower@gmail.com>
2025-11-04 23:18:45 +0100

    texture upload

--- scripts/textures.ts
+++ scripts/textures.ts
@@ -10,6 +10,12 @@ const pack = await loadImage(resolve(TEXTURES_FOLDERPATH,"pack.png"))
 
 const guiInventory = await loadImage(resolve(TEXTURES_GUI_FOLDERPATH,"inventory.png"))
 const guiStatusEffectBar = await loadImage(resolve(TEXTURES_GUI_FOLDERPATH,"status_effect_bar.png"))
+const guiHotbar = await loadImage(resolve(TEXTURES_GUI_FOLDERPATH,"hotbar.png"))
+const guiHotbarSelect = await loadImage(resolve(TEXTURES_GUI_FOLDERPATH,"hotbar_select.png"))
+const guiButton = await loadImage(resolve(TEXTURES_GUI_FOLDERPATH,"button.png"))
+const guiButtonActive = await loadImage(resolve(TEXTURES_GUI_FOLDERPATH,"button_active.png"))
+const guiButtonDisabled = await loadImage(resolve(TEXTURES_GUI_FOLDERPATH,"button_disabled.png"))
+const guiCrosshair = await loadImage(resolve(TEXTURES_GUI_FOLDERPATH,"crosshair.png"))
 
 //#endregion
 
@@ -20,6 +26,12 @@ namespace Textures
     {
         export const INVENTORY = guiInventory
         export const STATUS_EFFECT_BAR = guiStatusEffectBar
+        export const HOTBAR = guiHotbar
+        export const HOTBAR_SELECT = guiHotbarSelect
+        export const BUTTON = guiButton
+        export const BUTTON_ACTIVE = guiButtonActive
+        export const BUTTON_DISABLED = guiButtonDisabled
+        export const CROSSHAIR = guiCrosshair
     }
 }
 
